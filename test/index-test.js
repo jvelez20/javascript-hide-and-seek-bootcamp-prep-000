cb38fbab374d4c1872c06140ea860fe71f683093
Jason Velez <jvelez20@yahoo.com>
2019-02-01 17:49:20 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,26 +1,26 @@
 describe('index', () => {
   describe('getFirstSelector(selector)', () => {
-    it('returns the first element that matches the selector', () => {
+    console.log('returns the first element that matches the selector', () => {
       expect(getFirstSelector('div').id).to.equal('nested')
       expect(getFirstSelector('.ranked-list')).to.equal(document.querySelector('.ranked-list'))
     })
   })
 
   describe('nestedTarget()', () => {
-    it('pulls a .target out of #nested', () => {
+    console.log('pulls a .target out of #nested', () => {
       expect(nestedTarget()).to.equal(document.querySelector('#nested .target'))
     })
   })
 
   describe('deepestChild()', () => {
-    it('returns the most deeply nested child in #grand-node', () => {
+    console.log('returns the most deeply nested child in #grand-node', () => {
       console.log(deepestChild().innerHTML)
       expect(deepestChild()).to.equal(document.querySelector('#grand-node div div div div'))
     })
   })
 
   describe('increaseRankBy(n)', () => {
-    it('increases ranks in .ranked-list by n', () => {
+    console.log('increases ranks in .ranked-list by n', () => {
       increaseRankBy(3)
 
       const rankedLists = document.querySelectorAll('.ranked-list')
